54610a332545c0ae20fdc8917e26b0c991e045a3
Michał <151668327+MikiMaJe@users.noreply.github.com>
2024-02-16 20:22:01 +0100

    added colors

--- Project 4/js/script.js
+++ Project 4/js/script.js
@@ -9,6 +9,7 @@ const modal = document.querySelector(".modal-shadow");
 const infoBtn = document.querySelector(".fa-question");
 const time = document.querySelector(".time");
 const stopWatch = document.querySelector(".stopwatch");
+const ulList = document.querySelector(".saved-times");
 
 let seconds = 0;
 let minutes = 0;
@@ -16,6 +17,18 @@ let count;
 let countNumber = 1;
 
 const archiveSaved = [];
+
+//Zmiana kolorów
+
+const brushIco = document.querySelector(".fa-paintbrush");
+const colorPanel = document.querySelector(".wrapper__info__panel__colors");
+const colorBlue = document.querySelector(".color-blue");
+const colorRed = document.querySelector(".color-red");
+const colorGreen = document.querySelector(".color-green");
+
+const root = document.documentElement;
+
+//funkcje
 
 const startHandle = () => {
 	clearInterval(count);
@@ -53,7 +66,12 @@ const stopHandle = () => {
 const clearHandle = () => {
 	minutes = 0;
 	seconds = 0;
+	countNumber = 1;
 	stopWatch.innerHTML = ``;
+	archiveSaved.splice(0, archiveSaved.length);
+	while (ulList.hasChildNodes()) {
+		ulList.removeChild(ulList.firstChild);
+	}
 };
 
 const showArchive = () => {
@@ -61,7 +79,6 @@ const showArchive = () => {
 };
 
 const archiveMem = () => {
-	const ulList = document.querySelector(".saved-times");
 	const listEl = document.createElement("li");
 	archiveSaved.forEach((mem) => {
 		listEl.innerHTML = `<span>Pomiar numer ${countNumber} - </span>${mem}`;
@@ -74,6 +91,12 @@ const showModal = () => {
 	modal.classList.toggle("show");
 };
 
+//Funcke zmiany kolorów
+const showColorPanel = () => {
+	colorPanel.classList.toggle("show-flex");
+};
+
+//listenery główne
 startBtn.addEventListener("click", startHandle);
 pauseBtn.addEventListener("click", pauseHandle);
 stopBtn.addEventListener("click", stopHandle);
@@ -81,3 +104,15 @@ deleteBtn.addEventListener("click", clearHandle);
 archiveBtn.addEventListener("click", showArchive);
 infoBtn.addEventListener("click", showModal);
 modalCloseBtn.addEventListener("click", showModal);
+
+//Zmiana kolorów listenery
+brushIco.addEventListener("click", showColorPanel);
+colorBlue.addEventListener("click", () => {
+	root.style.setProperty("--theme-color", "royalblue");
+});
+colorRed.addEventListener("click", () => {
+	root.style.setProperty("--theme-color", "tomato");
+});
+colorGreen.addEventListener("click", () => {
+	root.style.setProperty("--theme-color", "lime");
+});
